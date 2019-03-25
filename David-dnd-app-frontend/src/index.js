0eed460a0d65e22b4576b058575f2d9a69770a73
David <dajohnson1998@gmail.com>
2019-03-25 10:49:28 -0400

    pushing to fix server

--- David-dnd-app-frontend/src/index.js
+++ David-dnd-app-frontend/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import store from '../../store';
-import App from '../../components/app';
+import store from './store';
+import App from './components/app';
 import './styles/index.css'
 
 ReactDOM.render(
